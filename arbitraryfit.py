e607745ff38701c3b9e3ac7451cd42773d146032
scott <scott@localhost.localdomain>
2014-12-19 09:19:48 -0500

    cleaned up some more

--- arbitraryfit.py
+++ arbitraryfit.py
@@ -7,28 +7,25 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fmin
 from scipy.stats import norm
 
-def arbitraryfit(x,y,color,function_of_array_c_and_variable_x,initial_c):
+def arbitraryfit(x,y,color,function_of_parameter_array_and_variable_x,parameters_guess):
     
     y = np.array(y)    
     x = np.array(x)
-    # parametric function, x is the independent variable
-    # and c are the parameters.
-    fp = function_of_array_c_and_variable_x
+
     # error function to minimize
-    e = lambda p, x, y: ((fp(p,x)-y)**2).sum()
+    error = lambda parameters, x, y: ((function_of_parameter_array_and_variable_x(parameters,x)-y)**2).sum()
 
     # fitting the data with fmin
-    p0 = initial_c # initial parameter value
-    p = fmin(e, p0, args=(x,y))
+    parameters = fmin(error, parameters_guess, args=(x,y))
 
-    print 'estimator parameters: ', p
+    print 'estimator parameters: ', parameters
 
     #change this so that your function fits nicely on screen... start, stop, count
     xx = np.linspace(0,16,1000)
     plt.plot(x,y,'bo')
-    plt.plot(xx, fp(p,xx),color, label = 'fit')
+    plt.plot(xx, fp(parameters,xx),color, label = 'fit')
     plt.legend()
-    return p
+    return parameters
 
 
 if __name__ == "__main__":
